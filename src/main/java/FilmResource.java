5f7b0c2b5dc7b3040fa13c266465695457217f40
leonardorafaelli <leonardorafaelli@gmail.com>
2024-04-19 17:54:39 +0200

    feat: film actors query with joining table

--- src/main/java/FilmResource.java
+++ src/main/java/FilmResource.java
@@ -1,3 +1,4 @@
+import io.vertx.codegen.annotations.ProxyClose;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -8,6 +9,7 @@ import repository.FilmRepository;
 
 import java.awt.*;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/")
 public class FilmResource {
@@ -28,6 +30,30 @@ public class FilmResource {
     public String getFilmById(short filmId){
         Optional<Film> optionalFilm = filmRepository.getFilmById(filmId);
         return optionalFilm.isPresent() ? optionalFilm.get().getTitle() : "No film was found!";
+    }
+
+
+    @GET
+    @Path("/film-page/{page}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String filmPage(long page, short minLength){
+        return filmRepository.getPageFilm(page, minLength)
+                .map(f -> String.format("%s (%d min)", f.getTitle(), f.getLength()))
+                .collect(Collectors.joining("\n"));
+    }
+
+    @GET
+    @Path("/actors/{startsWith}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String actors(String startsWith){
+        return filmRepository.actors(startsWith)
+                .map(f -> String.format("%s (%d min) - %s",
+                        f.getTitle(),
+                        f.getLength(),
+                        f.getActors().stream().map(
+                                a -> String.format("%s %s", a.getFirstName(), a.getLastName()))
+                                .collect(Collectors.joining(", "))
+                        )).collect(Collectors.joining("\n"));
     }
 
 }
--- src/main/java/repository/FilmRepository.java
+++ src/main/java/repository/FilmRepository.java
@@ -1,12 +1,18 @@
 package repository;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud;
 import com.speedment.jpastreamer.application.JPAStreamer;
+import com.speedment.jpastreamer.projection.Projection;
+import com.speedment.jpastreamer.streamconfiguration.StreamConfiguration;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.hibernate.sql.ast.tree.predicate.Junction;
 import org.quarkus_study.app.model.Film;
 import org.quarkus_study.app.model.Film$;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @ApplicationScoped
@@ -14,10 +20,32 @@ public class FilmRepository {
 
         @Inject
         JPAStreamer jpaStreamer;
-        // Optional - Cause it's gonna be populated, only if it finds a movie
+        // Optional - Because it's going to be populated, only if it finds a movie
+
+        private static final int PAGE_SIZE = 20;
+
         public Optional<Film> getFilmById(short filmId) {
             return jpaStreamer.stream(Film.class)
                     .filter(Film$.filmId.equal(filmId))
                     .findFirst();
         };
+
+        public Stream<Film> getPageFilm(long page, short minLength){
+                return jpaStreamer.stream(Projection.select(Film$.filmId, Film$.title, Film$.length))
+                        .filter(Film$.length.greaterThan(minLength))
+                        .sorted(Film$.length)
+                        .skip(page * PAGE_SIZE)
+                        .limit(PAGE_SIZE);
+        }
+
+
+        public Stream<Film> actors (String startsWith) {
+                final StreamConfiguration<Film> sc =
+                        StreamConfiguration.of(Film.class)
+                                .joining(Film$.actors);
+
+                return jpaStreamer.stream(sc)
+                        .filter(Film$.title.startsWith(startsWith))
+                        .sorted(Film$.length.reversed());
+        }
 }
